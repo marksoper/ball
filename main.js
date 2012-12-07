9a516c0c4669c0298540c72ac578ab8bea51d340
Mark Soper <masoper@gmail.com>
2012-12-07 15:32:59 -0500

    reworking main

--- main.js
+++ main.js
@@ -17,50 +17,44 @@
     this.context = this.context || this.el.getContext('2d');
   };
 
-  Canvas.prototype.resize = function() {
-    this.el.width  = window.innerWidth;
-    this.el.height = Math.floor(this.el.width / this.aspectRatio);
-    console.log("resizing to " + this.el.width + " by " + this.el.height);
+  var resizeCanvas = function(canvas, aspectRatio) {
+    canvas.width  = window.innerWidth;
+    canvas.height = Math.floor(canvas.width / aspectRatio);
+    console.log("resizing to " + canvas.width + " by " + canvas.height);
   };
 
-  Canvas.prototype.clear = function() {
-    this.context.clearRect(0, 0, this.el.width, this.el.height);
+  var clearCanvas = function(canvas, context) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
   };
 
 
-  var Ball = function(options) {
-    for (var prop in options) {
-      this[prop] = options[prop];
-    }
-    this.canvas = this.canvas || canvas;
-    this.radius = this.radius || 100;
-    this.x = this.x || Math.floor(this.canvas.el.width / 2);
+
+
+    this.x = this.x || ;
     this.y = this.y || Math.floor(this.canvas.el.height / 4);
-  };
 
-  Ball.prototype.locate = function() {
-    this.x = Math.floor(this.canvas.el.width / 2);
-    this.y = Math.floor(this.canvas.el.height / 4);
-  };
-
-  Ball.prototype.draw = function() {
-    var x, y, radius;
-    for (var i=0; i<10; i++) {
-      this.canvas.context.beginPath();
-      x = Math.floor(this.x + (20 + i/50) * Math.random());
-      y = Math.floor(this.y + (20+ i/50) * Math.random());
-      radius = Math.floor(this.radius +3 * Math.random());
-      this.canvas.context.arc(x,y,this.radius,0,Math.PI*2,true);
-      this.canvas.context.stroke();
-    }
-  };
 
   var main = function() {
-    canvasEl = document.getElementById('canvas');
-    canvas = new Canvas();
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
     canvas.resize();
     var loop = new Loop();
-    var ball = new Ball();
+    var ball = new Ball({
+      canvas: canvas,
+      context: context,
+      radius: 100,
+      locateX: function() {
+        Math.floor(this.canvas.width / 2);
+      },
+      locateY: function() {
+        Math.floor(this.canvas.height / 4);
+      },
+      strokeColor: "#ff0000",
+      lineWidth: 10,
+      drawingStyle: 
+      this.lineWidth = options.lineWidth || 10;
+      this.drawingFunction = options.drawingFunction || this.drawBasic;
+    });
     loop.register("redraw", function() {
       canvas.clear.call(canvas);
       ball.locate.call(ball);
